979e41fc957bae7743a7c4511ce44a9df6d2ee86
AfroMetal <radek.kwlsk@gmail.com>
2015-11-29 14:18:56 +0100

    toString method to print cards in hand

--- Poker_TexasH/src/pokertexasholdem/Hand.java
+++ Poker_TexasH/src/pokertexasholdem/Hand.java
@@ -15,5 +15,16 @@ public class Hand {
     public void setHand( Card[] hand ) {
         this.hand = hand;
     }
-
+    
+    @Override
+    public String toString() {
+        StringBuilder sBuilder = new StringBuilder();
+        for( int i = 0; i < hand.length; i++ ) {
+            sBuilder.append( hand[i].toString() );
+            if( i != hand.length - 1) {
+                sBuilder.append('\n');
+            }
+        }
+        return sBuilder.toString();
+    }
 }
