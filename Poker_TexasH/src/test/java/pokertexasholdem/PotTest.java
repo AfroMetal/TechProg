abc3a26142eb7799c1c01f6bac88c94e9c9f907b
oskam <oska.magdalena@gmail.com>
2015-12-21 14:40:14 +0100

    New tests added for Pot Class.

--- Poker_TexasH/src/test/java/pokertexasholdem/PotTest.java
+++ Poker_TexasH/src/test/java/pokertexasholdem/PotTest.java
@@ -2,16 +2,42 @@ package pokertexasholdem;
 
 import static org.junit.Assert.*;
 
+import java.net.Socket;
+import java.util.Set;
+
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class PotTest {
 
 	private static Pot pot;
+	private Set<Player> contributorsSet;
+	private static Player player,player1;
+	private static String name;
+	private static int money;
+	private static int actualMoney;
+	private static int smallBlind;
+	private static int bigBlind;
+	private static Socket socket = null;
+	private static int partialBet;
+	
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		name = "Player1";
+		money = 500;
+		actualMoney = money;
+		smallBlind = 10;
+		bigBlind = 20;
+		partialBet= 10;
+		
+	}
 
 	@Before
 	public void setUp() throws Exception {
 		pot = new Pot(0);
+		player = new Player(name, socket, money);
+		player1 = new Player(name, socket, money);
 	}
 
 	@Test
@@ -26,6 +52,17 @@ public class PotTest {
 
 		pot.setPot(40);
 		assertEquals(40, pot.getBet());
+		
+		pot.addContributor(player);
+		assertNotNull(pot.hasContributed(player));
+		assertNotNull(pot.split(player, partialBet));
+		
+		pot.addContributor(player1);
+		assertEquals(20, pot.getValue());
+		
+		assertNotNull(pot.getContributorsSet());
+		
+		assertEquals("Pot: 20", pot.toString());
 	}
 
 }
