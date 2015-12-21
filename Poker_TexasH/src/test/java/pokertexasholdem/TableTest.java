1d8ef30c295f2b5e398449d2cbaf219f55ee89c7
oskam <oska.magdalena@gmail.com>
2015-12-21 16:02:36 +0100

    Test Class for Table

---
+++ Poker_TexasH/src/test/java/pokertexasholdem/TableTest.java
@@ -0,0 +1,56 @@
+package pokertexasholdem;
+
+import static org.junit.Assert.*;
+
+import java.net.Socket;
+import java.util.HashMap;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class TableTest {
+	
+	private static Table table;
+	private static int money;
+	private static int smallBlind;
+	private static HashMap<Socket, String> playersSocketsNames;
+	private static int activePlayersNum;
+	
+	
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		playersSocketsNames = new  HashMap<Socket, String>();
+		playersSocketsNames.put(null, "A");
+		playersSocketsNames.put(null, "B");
+		
+		money = 100;
+		smallBlind = 20;
+		
+		activePlayersNum = 5;
+	}
+
+	@Test
+	public void test() {
+		table = new Table(playersSocketsNames, money, smallBlind);
+		
+		table.run();
+		
+		assertNotNull(table.getPlayersList());
+		assertNotNull(table.getSmallBlind());
+		assertEquals(20, table.getSmallBlind());
+		assertNotNull(table.getBigBlind());
+		assertEquals(40, table.getBigBlind());
+		//assertNotNull(table.getDeck());
+		assertNotNull(table.getBoard());
+		assertNotNull(table.getDealer());
+		assertNotNull(table.getDealerPosition());
+		assertNotNull(table.getActivePlayers());
+		//assertNotNull(table.getActor());
+		assertNotNull(table.getActorPosition());
+		assertNotNull(table.getPots());
+		assertNotNull(table.getMinBet());
+		assertNotNull(table.getBet());
+		
+	}
+
+}
